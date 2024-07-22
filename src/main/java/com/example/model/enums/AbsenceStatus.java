b3dfebaf54840ee3b779a82341f3a90bc0724b00
Ivaylo37 <ivailo.vassilev99@gmail.com>
2024-07-22 11:26:30 +0300

    Remove exising files

--- src/main/java/com/example/model/enums/AbsenceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.model.enums;
-
-public enum AbsenceStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
--- src/main/java/com/example/repository/AbsenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Absence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AbsenceRepository extends JpaRepository<Absence, Long> {
-
-
-}
